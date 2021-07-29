253259bb6acb0de7a041263691ac7238cdd699c8
maklad42 <maklad42+git@gmail.com>
2021-07-29 11:26:54 +0900

    Adds three-sphere and three-scene js files to create the globe on the basic page. 210729

--- src/components/three-scene.js
+++ src/components/three-scene.js
@@ -1,3 +1,31 @@
 import React from "react"
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
+import ThreeSphere from "./three-sphere"
+import ThreeGeo from "./three-geo"
+
+const ThreeScene = () => {
+  return (
+    <Canvas
+      gl={{ antialias: false, alpha: false }}
+      camera={{
+        fov: 45,
+        position: [0, 0, 300],
+      }}
+      onCreated={({ gl }) => {
+        gl.setClearColor("#ffffff")
+      }}
+      style={{
+        width: "100vw",
+        height: "100vh",
+        cursor: "move",
+      }}
+    >
+      <OrbitControls enableRotate={true} enableZone={false} enablePan={false} />
+      <ThreeSphere />
+      <ThreeGeo />
+    </Canvas>
+  )
+}
+
+export default ThreeScene
---
+++ src/components/three-sphere.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ThreeSphere = () => {
+  return (
+    <mesh>
+      <sphereGeometry args={[100, 32, 32]} />
+      <meshBasicMaterial color="#f7f7f7" transparent={true} opacity={0.6} />
+    </mesh>
+  )
+}
+
+export default ThreeSphere
